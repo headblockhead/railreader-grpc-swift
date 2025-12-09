a80281ef7a1714d4138db44ebecd94daaf515e32
Edward Hesketh <inbox@edwardh.dev>
2025-12-09 16:36:44 +0000

    add correct dependencies

--- Package.swift
+++ Package.swift
@@ -10,9 +10,17 @@ let package = Package(
             targets: ["RailReaderGRPC"]
         ),
     ],
+    dependencies: [
+        .package(url: "https://github.com/grpc/grpc-swift-2.git", from: "2.0.0"),
+        .package(url: "https://github.com/grpc/grpc-swift-protobuf.git", from: "2.0.0"),
+    ],
     targets: [
         .target(
-            name: "RailReaderGRPC"
+            name: "RailReaderGRPC",
+            dependencies: [
+                .product(name: "GRPCCore", package: "grpc-swift-2"),
+                .product(name: "GRPCProtobuf", package: "grpc-swift-protobuf"),
+            ]
         ),
     ]
 )
