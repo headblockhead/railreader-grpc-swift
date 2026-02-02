a84a7b881c833eabaa4ca65250f08c85d87e6916
Edward Hesketh <inbox@edwardh.dev>
2026-02-02 00:27:41 +0000

    ci: fully regenerate code every time

--- gen.sh
+++ gen.sh
@@ -1,1 +1,2 @@
+rm -f ./Sources/RailReaderGRPC/*.{pb,grpc}.swift
 protoc --swift_out=./Sources/RailReaderGRPC --grpc-swift-2_out=./Sources/RailReaderGRPC --swift_opt=Visibility=Public --grpc-swift-2_opt=Visibility=Public ./railreader-grpc/*.proto -I./railreader-grpc
