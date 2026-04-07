09ad1f3f925709607fbd846a033712dc45c7121d
Edward Hesketh <inbox@edwardh.dev>
2026-04-07 23:04:47 +0100

    fix: autoupdating when regen

--- gen.sh
+++ gen.sh
@@ -1,2 +1,3 @@
 rm -f ./Sources/RailReaderGRPC/*.{pb,grpc}.swift
+swift package update
 protoc --swift_out=./Sources/RailReaderGRPC --grpc-swift-2_out=./Sources/RailReaderGRPC --swift_opt=Visibility=Public --grpc-swift-2_opt=Visibility=Public ./railreader-grpc/*.proto -I./railreader-grpc
